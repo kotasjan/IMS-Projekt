198c553b39d704c6623ac88fc8c3b67cf3bac5f8
Kotas Jan <xkotas07@merlin.fit.vutbr.cz>
2017-12-04 18:25:32 +0100

    Pridana podpora vice zarizeni, uprava vyberu vstupnich kusu serie. Pridano par komentu.

--- firmaHridele.cpp
+++ firmaHridele.cpp
@@ -12,32 +12,58 @@
 
 using namespace std;
 
-const int PRACOVNI_DOBA = 8;
-const int POCET_ZAMESTNANCU = 3;
-const int SERIE_KUSU = 5;
-const int POCET_GENEROVANYCH_KUSU = 200;
-
-// nemenit - program nepodporuje nepodporuje vice (zatim)
-// const int POCET_VRTACEK = 1;
-// const int POCET_SOUSTRUHU = 1;
-// const int POCET_FREZ = 1;
-// const int POCET_BRUSEK = 1;
-
-const int DOVOLENA = 29;  // 5 tydnu => 25 dnu (5 dni z toho je celozavodka) + 9 statni svatek
-// pravdepodobnost, ze si zamestnanec v konkretni den vezme dovolenou
-const double PRAVDEPODOBNOST_DOVOLENE = 0.105;
-
+// Dulezite konstanty pro efektivnejsi praci s casem a lepsi abstrakci.
 const int MINUTA = 60;
 const int HODINA = 60 * MINUTA;
 const int DEN = 24 * HODINA;
 const int ROK = 365 * DEN;
 
-const double SIMULACNI_DOBA = 5 * ROK;  // Mozne nastavit dle potreby
+// Nastaveni poctu zamestnancu pracujicich ve firme
+const int POCET_ZAMESTNANCU = 4;
 
-Facility f_vrtacka;
-Facility f_soustruh;
-Facility f_freza;
-Facility f_bruska;
+/*
+ * Serie kusu predstavuje mnozstvi kusu, které zamestnanec vyrobi
+ * za sebou. Tzn. zamestnanec si z fronty vezme tento pocet kusu
+ * a dokud je neudela, nemuze zmenit cinnost ani jit na prestavku.
+ * Jedinou vyjimkou je konec pracovni doby.
+*/
+const int SERIE_KUSU = 5;
+
+// Pocet generovanych surovych kusu za jednotku casu (perioda generovani).
+const int POCET_GENEROVANYCH_KUSU = 300;
+
+// Perioda generovani predstavuje periodu dodavky surovych kusu z dodavatelske firmy.
+const int PERIODA_GENEROVANI = 7 * DEN;
+
+// Nastaveni poctu zarizeni.
+const int POCET_VRTACEK = 2;
+const int POCET_SOUSTRUHU = 1;
+const int POCET_FREZ = 1;
+const int POCET_BRUSEK = 1;
+
+// 5 tydnu => 25 dnu (5 dni z toho je celozavodka) + 9 statni svatek.
+const int DOVOLENA = 29;
+
+/*
+ * Pravdepodobnost, ze si zamestnanec v konkretni den vezme dovolenou. Je to
+ * vypocitana hodnota 
+*/
+const double PRAVDEPODOBNOST_DOVOLENE = 0.105;
+
+// Delka pracovni doby.
+const int PRACOVNI_DOBA = 8 * HODINA;
+
+// Simulacni doba predstavuje delku simulace v simulacnim case.
+const double SIMULACNI_DOBA = 1 * ROK;
+
+
+/*
+ * Zarizeni, ktere obstaravaji obsluhu.
+*/
+Facility f_vrtacka[POCET_VRTACEK];
+Facility f_soustruh[POCET_SOUSTRUHU];
+Facility f_freza[POCET_FREZ];
+Facility f_bruska[POCET_BRUSEK];
 
 Queue q_zamestnanci;
 Queue q_surove_kusy;
@@ -49,7 +75,8 @@ Queue q_hridele;
 bool den = false;
 bool volno = false;
 bool celozavodka = false;
-bool debug = false;
+bool end_simulation = false;
+bool debug = true;
 
 int days = 0;
 int celkovyPocetHrideli = 0;
@@ -63,7 +90,6 @@ int pocet_pracujicich_zamestnancu = POCET_ZAMESTNANCU;
 // TStat delka_fronty_vyfrezovanych_kusu("Delka fronty vyfrezovanych kusu", 0);
 // TStat delka_fronty_vybrousenych_kusu("Delka fronty vybrousenych kusu", 0);
 // TStat pocet_vyrobenych_hrideli("Pocet vyrobenych hrideli", 0);
-
 
 class HRIDEL : public Process {
   void Behavior() {
@@ -184,6 +210,7 @@ class SUROVY_KUS : public Process {
 
 class ZAMESTNANEC : public Process {
   int dovolena;
+  int pocet_hotovych_kusu = 0;
 
   void Behavior() {
     while (1) {
@@ -206,56 +233,87 @@ class ZAMESTNANEC : public Process {
         while (1) {
           if (den && !volno) {
             if (q_vyfrezovane_kusy.Length() >= 5) {
-              if (!f_bruska.Busy()) {
-                Seize(f_bruska);
+              for (int j = 0; j < POCET_BRUSEK; j++) {
+                if (!f_bruska[j].Busy()) {
+                  Seize(f_bruska[j]);
 
-                this->Brouseni();
+                  this->Brouseni();
 
-                Release(f_bruska);
+                  Release(f_bruska[j]);
+                  break;
+                }
               }
             } else if (q_vysoustruzene_kusy.Length() >= 5) {
-              if (!f_freza.Busy()) {
-                Seize(f_freza);
+              for (int j = 0; j < POCET_FREZ; j++) {
+                if (!f_freza[j].Busy()) {
+                  Seize(f_freza[j]);
 
-                this->Frezovani();
+                  this->Frezovani();
 
-                Release(f_freza);
+                  Release(f_freza[j]);
+                  break;
+                }
               }
             } else if (q_vyvrtane_kusy.Length() >= 5) {
-              if (!f_soustruh.Busy()) {
-                Seize(f_soustruh);
+              for (int j = 0; j < POCET_SOUSTRUHU; j++) {
+                if (!f_soustruh[j].Busy()) {
+                  Seize(f_soustruh[j]);
 
-                this->Soustruzeni();
+                  this->Soustruzeni();
 
-                Release(f_soustruh);
+                  Release(f_soustruh[j]);
+                  break;
+                }
               }
             } else if (q_surove_kusy.Length() >= 5) {
-              if (!f_vrtacka.Busy()) {
-                Seize(f_vrtacka);
+              for (int j = 0; j < POCET_VRTACEK; j++) {
+                if (!f_vrtacka[j].Busy()) {
+                  Seize(f_vrtacka[j]);
 
-                this->Vrtani();
+                  this->Vrtani();
 
-                Release(f_vrtacka);
+                  Release(f_vrtacka[j]);
+                  break;
+                }
               }
             }
           } else {
             q_zamestnanci.Insert(this);
             break;
           }
-          Wait(5);
+
+          if (pocet_hotovych_kusu != 0 && den) {
+            if (Random() <= 0.5) {
+              Wait(Exponential(5 * MINUTA));
+            }
+            pocet_hotovych_kusu = 0;
+          } else {
+            Wait(5);
+          }
+
+          if(Time >= SIMULACNI_DOBA - 2*HODINA) this->Cancel();
         }
       }
+      pocet_hotovych_kusu = 0;
       Passivate();
     }
   }
 
   void Vrtani() {
     int pocet_zmetku = 0;
-    int pocet_hotovych_kusu = 0;
 
-    while (pocet_hotovych_kusu < 5) {
-      if (!q_surove_kusy.Empty()) {
-        SUROVY_KUS *k = (SUROVY_KUS *)q_surove_kusy.GetFirst();
+    Queue pom_q;
+
+    while (pom_q.Length() != SERIE_KUSU) {
+      if (!q_surove_kusy.Empty())
+        pom_q.Insert(q_surove_kusy.GetFirst());
+      else
+        Wait(5);
+    }
+
+    while (pom_q.Length() != 0) {
+      if (!pom_q.Empty()) {
+        SUROVY_KUS *k = (SUROVY_KUS *)pom_q.GetFirst();
         k->Activate();
 
         // provadeni prace na stroji
@@ -271,11 +329,15 @@ class ZAMESTNANEC : public Process {
           pocet_zmetku++;
           k->Cancel();
         }
-      } else {
-        Wait(5);
       }
 
-      if (!den) break;
+      if (!den) {
+        while (!pom_q.Empty()){
+          (pom_q.GetFirst())->Cancel();
+          (new SUROVY_KUS)->Activate();
+        }
+        break;
+      }
     }
 
     if (debug) {
@@ -294,19 +356,23 @@ class ZAMESTNANEC : public Process {
            << "     ::  "
            << "Vyvrtáno " << to_string(pocet_hotovych_kusu) << " kusů" << endl;
     }
-
-    if (pocet_hotovych_kusu == 5 && den) {
-      if (Random() <= 0.5) Wait(Exponential(5 * MINUTA));
-    }
   }
 
   void Soustruzeni() {
     int pocet_zmetku = 0;
-    int pocet_hotovych_kusu = 0;
 
-    while (pocet_hotovych_kusu < 5) {
-      if (!q_vyvrtane_kusy.Empty()) {
-        VYVRTANY_KUS *k = (VYVRTANY_KUS *)q_vyvrtane_kusy.GetFirst();
+    Queue pom_q;
+
+    while (pom_q.Length() != SERIE_KUSU) {
+      if (!q_vyvrtane_kusy.Empty())
+        pom_q.Insert(q_vyvrtane_kusy.GetFirst());
+      else
+        Wait(5);
+    }
+
+    while (pom_q.Length() != 0) {
+      if (!pom_q.Empty()) {
+        VYVRTANY_KUS *k = (VYVRTANY_KUS *)pom_q.GetFirst();
         k->Activate();
 
         // provadeni prace na stroji
@@ -326,7 +392,13 @@ class ZAMESTNANEC : public Process {
         Wait(5);
       }
 
-      if (!den) break;
+      if (!den) {
+        while (!pom_q.Empty()){
+          (pom_q.GetFirst())->Cancel();
+          (new VYVRTANY_KUS)->Activate();
+        }
+        break;
+      }
     }
 
     if (debug) {
@@ -346,20 +418,23 @@ class ZAMESTNANEC : public Process {
            << "Vysoustruženo " << to_string(pocet_hotovych_kusu) << " kusů"
            << endl;
     }
-
-    if (pocet_hotovych_kusu == 5 && den) {
-      if (Random() <= 0.5) Wait(Exponential(5 * MINUTA));
-    }
   }
 
   void Frezovani() {
     int pocet_zmetku = 0;
-    int pocet_hotovych_kusu = 0;
 
-    while (pocet_hotovych_kusu < 5) {
-      if (!q_vysoustruzene_kusy.Empty()) {
-        VYSOUSTRUZENY_KUS *k =
-            (VYSOUSTRUZENY_KUS *)q_vysoustruzene_kusy.GetFirst();
+    Queue pom_q;
+
+    while (pom_q.Length() != SERIE_KUSU) {
+      if (!q_vysoustruzene_kusy.Empty())
+        pom_q.Insert(q_vysoustruzene_kusy.GetFirst());
+      else
+        Wait(5);
+    }
+
+    while (pom_q.Length() != 0) {
+      if (!pom_q.Empty()) {
+        VYSOUSTRUZENY_KUS *k = (VYSOUSTRUZENY_KUS *)pom_q.GetFirst();
         k->Activate();
 
         // provadeni prace na stroji
@@ -379,7 +454,13 @@ class ZAMESTNANEC : public Process {
         Wait(5);
       }
 
-      if (!den) break;
+      if (!den) {
+        while (!pom_q.Empty()){
+          (pom_q.GetFirst())->Cancel();
+          (new VYSOUSTRUZENY_KUS)->Activate();
+        }
+        break;
+      }
     }
 
     if (debug) {
@@ -399,19 +480,23 @@ class ZAMESTNANEC : public Process {
            << "Vyfrézováno " << to_string(pocet_hotovych_kusu) << " kusů"
            << endl;
     }
-
-    if (pocet_hotovych_kusu == 5 && den) {
-      if (Random() <= 0.5) Wait(Exponential(5 * MINUTA));
-    }
   }
 
   void Brouseni() {
     int pocet_zmetku = 0;
-    int pocet_hotovych_kusu = 0;
 
-    while (pocet_hotovych_kusu < 5) {
-      if (!q_vyfrezovane_kusy.Empty()) {
-        VYFREZOVANY_KUS *k = (VYFREZOVANY_KUS *)q_vyfrezovane_kusy.GetFirst();
+    Queue pom_q;
+
+    while (pom_q.Length() != SERIE_KUSU) {
+      if (!q_vyfrezovane_kusy.Empty())
+        pom_q.Insert(q_vyfrezovane_kusy.GetFirst());
+      else
+        Wait(5);
+    }
+
+    while (pom_q.Length() != 0) {
+      if (!pom_q.Empty()) {
+        VYFREZOVANY_KUS *k = (VYFREZOVANY_KUS *)pom_q.GetFirst();
         k->Activate();
 
         // provadeni prace na stroji
@@ -431,7 +516,13 @@ class ZAMESTNANEC : public Process {
         Wait(5);
       }
 
-      if (!den) break;
+      if (!den) {
+        while (!pom_q.Empty()){
+          (pom_q.GetFirst())->Cancel();
+          (new VYFREZOVANY_KUS)->Activate();
+        }
+        break;
+      }
     }
 
     if (debug) {
@@ -450,10 +541,6 @@ class ZAMESTNANEC : public Process {
            << "     ::  "
            << "Vybroušeno " << to_string(pocet_hotovych_kusu) << " hřídelí"
            << endl;
-    }
-
-    if (pocet_hotovych_kusu == 5 && den) {
-      if (Random() <= 0.5) Wait(Exponential(5 * MINUTA));
     }
   }
 
@@ -494,7 +581,7 @@ class GEN_SUROVYCH_KUSU : public Event {
                " surových kusů.")
            << endl;
     }
-    Activate(Time + (7 * DEN + 1));
+    Activate(Time + PERIODA_GENEROVANI);
   }
 };
 
@@ -529,8 +616,8 @@ class GEN_VOLNO : public Event {
 
 class GEN_CELOZAVODKA : public Event {
   void Behavior() {
-    if (!celozavodka && (volno || (days % 7 != 1))){
-      Activate(Time + (DEN));
+    if (!celozavodka && (volno || (days % 7 != 1))) {
+      if(!end_simulation) Activate(Time + (DEN));
     } else if (!volno && !celozavodka) {
       celozavodka = true;
       (new GEN_VOLNO(5 * DEN - 1))->Activate();
@@ -542,7 +629,7 @@ class GEN_CELOZAVODKA : public Event {
              << (int)(t % HODINA) / MINUTA << ":" << setw(2) << (t % MINUTA)
              << "     ::  " << ("Celozávodka započata.") << endl;
       }
-      Activate(Time + (5 * DEN));
+      if(!end_simulation) Activate(Time + (5 * DEN));
     } else {
       celozavodka = false;
     }
@@ -551,12 +638,11 @@ class GEN_CELOZAVODKA : public Event {
 
 class GEN_DEN : public Event {
   void Behavior() {
-
     // v sobotu a nedeli se nepracuje
     if ((days % 7 == 6 || (days % 7 == 0 && days != 0)) && !den) {
-      (new GEN_VOLNO(DEN - 1))->Activate();
+      if(!end_simulation) (new GEN_VOLNO(DEN - 1))->Activate();
       days++;
-      
+
       if (debug && days != 0) {
         cout << "--------------------------------------------------------------"
                 "---------"
@@ -598,7 +684,7 @@ class GEN_DEN : public Event {
       }
 
       // jednou za rok je zastavena cinnost firmy a je vyhlasena celozavodka
-      if (days % 365 == 358) (new GEN_CELOZAVODKA)->Activate();
+      if (days % 365 == 358 && !end_simulation) (new GEN_CELOZAVODKA)->Activate();
 
       den = true;
       days++;
@@ -618,21 +704,76 @@ class GEN_DEN : public Event {
           }
         }
       }
-      Activate(Time + (8 * HODINA));
+      if(Time < SIMULACNI_DOBA - PRACOVNI_DOBA) Activate(Time + (PRACOVNI_DOBA));
+
     } else {  // pokud je konec pracovniho dne, nastav volno na 16h
       den = false;
-      Activate(Time + (16 * HODINA));
+      if(!end_simulation) Activate(Time + (DEN - PRACOVNI_DOBA));
     }
+  }
+};
+
+class GEN_END : public Event {
+  void Behavior() {
+
+    if (end_simulation) {
+      while (!q_surove_kusy.Empty()){
+        (q_surove_kusy.GetFirst())->Cancel();
+      }
+
+      while (!q_vyvrtane_kusy.Empty()){
+        (q_vyvrtane_kusy.GetFirst())->Cancel();
+      }
+
+      while (!q_vysoustruzene_kusy.Empty()){
+        (q_vysoustruzene_kusy.GetFirst())->Cancel();
+      }
+
+      while (!q_vyfrezovane_kusy.Empty()){
+        (q_vyfrezovane_kusy.GetFirst())->Cancel();
+      }
+
+      while (!q_hridele.Empty()){
+        (q_hridele.GetFirst())->Cancel();
+      }
+
+      while (!q_zamestnanci.Empty()){
+        (q_zamestnanci.GetFirst())->Cancel();
+      }
+
+  } else {
+    end_simulation = true;
+    Activate(Time + DEN - 1);
+  }
   }
 };
 
 int main(int argc, char **argv) {
   RandomSeed(time(NULL));
 
-  if(debug) SetOutput("stats.out");
+  if (debug) SetOutput("stats.out");
 
   // inicializace experimentu
   Init(0, SIMULACNI_DOBA);
+
+  // nastaveni nazvu zarizeni
+  for (int i = 0; i < POCET_VRTACEK; i++) {
+    f_vrtacka[i].SetName(("Vrtacka " + to_string(i + 1)).c_str());
+  }
+
+  for (int i = 0; i < POCET_SOUSTRUHU; i++) {
+    f_soustruh[i].SetName(("Soustruh " + to_string(i + 1)).c_str());
+  }
+
+  for (int i = 0; i < POCET_FREZ; i++) {
+    f_freza[i].SetName(("Freza " + to_string(i + 1)).c_str());
+  }
+
+  for (int i = 0; i < POCET_BRUSEK; i++) {
+    f_bruska[i].SetName(("Bruska " + to_string(i + 1)).c_str());
+  }
+
+  (new GEN_END)->Activate(SIMULACNI_DOBA - DEN);
 
   // vygeneruj zamestnance
   (new GEN_ZAMESTNANCU)->Activate();
@@ -640,24 +781,27 @@ int main(int argc, char **argv) {
   // aktivace udalosti zmen dne
   (new GEN_DEN)->Activate();
 
-    // vygeneruj zamestnance
+  // vygeneruj zamestnance
   (new GEN_SUROVYCH_KUSU)->Activate();
 
   // simulace
   Run();
 
-//  if (debug) {
-    unsigned long long t = Time;
-    cout << setw(3) << (int)t / ROK << "r, " << setw(3) << (int)(t % ROK) / DEN
-         << "d,  " << setw(2) << (int)(t % DEN) / HODINA << ":" << setw(2)
-         << (int)(t % HODINA) / MINUTA << ":" << setw(2) << (t % MINUTA)
-         << "     ::  " << ("Celkovy pocet vyrobenych hrideli = " +
-                            to_string(celkovyPocetHrideli))
-         << endl;
-// }
+  unsigned long long t = Time;
+  cout << setw(3) << (int)t / ROK << "r, " << setw(3) << (int)(t % ROK) / DEN
+       << "d,  " << setw(2) << (int)(t % DEN) / HODINA << ":" << setw(2)
+       << (int)(t % HODINA) / MINUTA << ":" << setw(2) << (t % MINUTA)
+       << "     ::  " << ("Celkovy pocet vyrobenych hrideli = " +
+                          to_string(celkovyPocetHrideli))
+       << endl;
 
   // tisk statistik
-  // odbaveni_letadla.Output();
-  // cekani_bezp.Output();
-  // odbaveni_zakaznika.Output();
+
+  for (int i = 0; i < POCET_VRTACEK; i++) f_vrtacka[i].Output();
+
+  for (int i = 0; i < POCET_SOUSTRUHU; i++) f_soustruh[i].Output();
+
+  for (int i = 0; i < POCET_FREZ; i++) f_freza[i].Output();
+
+  for (int i = 0; i < POCET_BRUSEK; i++) f_bruska[i].Output();
 }
